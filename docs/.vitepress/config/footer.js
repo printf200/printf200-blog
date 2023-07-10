4de9e222294305c28f710b21ed009de3f694a477
王天昊 <wangth@qcubic.com>
2023-07-10 13:17:53 +0800

    修改footer

--- docs/.vitepress/config/footer.js
+++ docs/.vitepress/config/footer.js
@@ -1,3 +1,3 @@
 export const footer = {
-    copyright: '辽ICP备19004191号-2',
+    copyright: 'Copyright ©2023-2024 printf200 版权所有 <a href="https://beian.miit.gov.cn/" target="_blank">京ICP备2023017333号-1</a>'
 }
--- docs/.vitepress/config/nav.js
+++ docs/.vitepress/config/nav.js
@@ -1,7 +1,7 @@
 const nav = [
 	{ text: '首页', link: '/' },
-	{ text: '学习日记', link: '/study/index' },
-	{ text: '小工具', link: '/tool/index' },
+	{ text: '学习日记', link: '/study/关于我' },
+	{ text: '小工具', link: '/tool/关于工具' },
 	{ text: 'github', link: 'https://github.com/printf200/printf200-blog/tree/master' },
 ]
 
